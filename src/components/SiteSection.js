c1873b7747466c2c08f9e603b8d4bc6b7faa8b18
dougajmcdonald <dougajmcdonald@gmail.com>
2017-05-15 20:44:21 +0100

    Layout tweaks

--- src/components/SiteSection.js
+++ src/components/SiteSection.js
@@ -9,7 +9,8 @@ class SiteSection extends React.Component {
       <div>
         <div className="sitesection-angled-bottom" styleName="sitesection-angled-bottom"></div>
         <div className="sitesection-component" styleName="sitesection-component">
-          <h2><i className="fa fa-lg fa-clock-o"></i> Time &amp; Date</h2>
+          <h2><i className="fa fa-clock-o"></i> Time &amp; Date</h2>
+          <p>We're getting married on the <b>30th of June 2018</b> and we'd love to see you there.</p>
         </div>
       </div>
     );
